6772680f48a39e65489e9bda82e4148839ba07bf
Patrick Jackman <patrickjackman@gmail.com>
2017-01-08 10:12:44 -0500

    DoIt First

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Patrick Jackman on 1/8/17.
+//  Copyright © 2017 Patrick Jackman. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name : String = ""
+    var important : Bool = false
+}
